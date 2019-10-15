05885f518516520b77eadee7ee67f81d778ec999
vikas-ambab <vikas.gupta@ambab.com>
2019-10-15 19:16:47 +0530

    some packages installed and coding

--- src/pages/Users/AddUsers.js
+++ src/pages/Users/AddUsers.js
@@ -1,13 +1,14 @@
 import React from "react";
 import UserForm from "./UserForm";
+import { format } from "date-fns";
 
 class AddUsers extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+
     this.state = {
       first_name: "",
       last_name: "",
@@ -15,42 +16,54 @@ class AddUsers extends React.Component {
       gender: "",
       hob: [],
       edu: "",
+      eduvalue: "",
+      npa_date: ""
     };
   }
 
+  handleDate = (name, date) => {
+    const formatedDate = date ? format(date, "yyyy-MM-dd") : "";
+    this.setState({ [name]: formatedDate });
+  };
+
   handleChange = e => {
-    if (event.target.type !== 'checkbox') {
+    if (e.target.type !== "checkbox") {
       this.setState({ [e.target.name]: e.target.value });
     } else {
-      const checkeds = document.getElementsByName('hob');
+      const checkeds = document.getElementsByName(e.target.name);
       let checkedArr = [];
       for (let i = 0; i < checkeds.length; i++) {
         if (checkeds[i].checked) {
           checkedArr.push(checkeds[i].value);
         }
       }
-      this.setState({hob: checkedArr});
+      this.setState({ [e.target.name]: checkedArr });
     }
-  }
+  };
 
-
-  handleSelectChange = (e,componentName) =>{
-    this.setState({ [componentName.name]:e.value});
-  }
+  handleSelectChange = (e, componentName) => {
+    this.setState({
+      [componentName.name]: e,
+      [componentName.name + "value"]: e.value
+    });
+  };
 
   handleSubmit = e => {
     e.preventDefault();
     // this.props.handleSubmit(data);
     console.log(this.state);
-  }
+  };
 
   render() {
-    return <UserForm 
-              statedata={this.state} 
-              changefunct={this.handleChange}
-              SelectChange={this.handleSelectChange} 
-              submithandler={this.handleSubmit}  
-            />;
+    return (
+      <UserForm
+        statedata={this.state}
+        changefunct={this.handleChange}
+        SelectChange={this.handleSelectChange}
+        handleDate={this.handleDate}
+        submithandler={this.handleSubmit}
+      />
+    );
   }
 }
 
--- src/pages/Users/UserForm.js
+++ src/pages/Users/UserForm.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Form, FormGroup, Input, Label, Row, Col } from "reactstrap";
-import Select from 'react-select';
+import Select from "react-select";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// import { parse } from "date-fns";
 
 const checkboxes = [
   {
@@ -16,111 +19,146 @@ const checkboxes = [
 ];
 
 const options = [
-  { value: 'ssc', label: 'Ssc' },
-  { value: 'hsc', label: 'Hsc' },
-  { value: 'ty', label: 'Ty' },
+  { value: "ssc", label: "Ssc" },
+  { value: "hsc", label: "Hsc" },
+  { value: "ty", label: "Ty" }
 ];
 
-const UserForm=(props)=>{
-	return(
-      <Form onSubmit={props.submithandler}>
-        <Row>
-          <Col md={4}>
-            <FormGroup>
-              <Label for="first_name">First Name</Label>
-              <Input
-                type="text"
-                name="first_name"
-                id="first_name"
-                placeholder="First Name"
-                value={props.statedata.first_name}
-                onChange={props.changefunct}
-              />
-            </FormGroup>
-          </Col>
-          <Col md={4}>
-            <FormGroup>
-              <Label for="last_name">Last Name</Label>
-              <Input
-                type="text"
-                name="last_name"
-                id="last_name"
-                placeholder="Last Name"
-                value={props.statedata.last_name}
-                onChange={props.changefunct}
-              />
-            </FormGroup>
-          </Col>
-          <Col md={4}>
-            <FormGroup>
-              <Label for="email">Email</Label>
-              <Input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email-Id "
-                value={props.statedata.email}
-                onChange={props.changefunct}
-              />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={4}>
-            <FormGroup>
-              <Label for="gender">Gender</Label>
-              <br />
-              <Input
-                type="radio"
-                name="gender"
-                value="male"
-                checked={props.statedata.gender === "male"}
-                onChange={props.changefunct}
-              />
-              Male
-              <Input
-                type="radio"
-                name="gender"
-                value="female"
-                checked={props.statedata.gender === "female"}
-                onChange={props.changefunct}
-              />
-              Female
-            </FormGroup>
-          </Col>
-          <Col md={4}>
-            <FormGroup>
-              <Label for="hob">Hob</Label>
-              <br />
-              {checkboxes.map(item => (
-                <label>
-                  {item.label}
-                  <Input
-                    type="checkbox"
-                    name={item.name}
-                    value={item.value}
-                    // checked={props.statedata.checkedItems.get(item.name)}
-                    onChange={props.changefunct}
-                  />
-                </label>
-              ))}
-            </FormGroup>
-          </Col>
-          <Col md={4}>
+const UserForm = props => {
+  const [city, setCity] = useState("");
+
+  useEffect(() => {});
+
+  return (
+    <Form onSubmit={props.submithandler}>
+      <Row>
+        <Col md={4}>
           <FormGroup>
-              <Label for="edu">Edu</Label>
-              <Select
-                name="edu"
-                value={props.statedata.edu}
-                onChange={props.SelectChange  }
-                options={options}
-              />
+            <Label for="first_name">First Name</Label>
+            <Input
+              type="text"
+              name="first_name"
+              id="first_name"
+              placeholder="First Name"
+              value={props.statedata.first_name}
+              onChange={props.changefunct}
+            />
           </FormGroup>
-          </Col>
-        </Row>
+        </Col>
+        <Col md={4}>
+          <FormGroup>
+            <Label for="last_name">Last Name</Label>
+            <Input
+              type="text"
+              name="last_name"
+              id="last_name"
+              placeholder="Last Name"
+              value={props.statedata.last_name}
+              onChange={props.changefunct}
+            />
+          </FormGroup>
+        </Col>
+        <Col md={4}>
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email-Id "
+              value={props.statedata.email}
+              onChange={props.changefunct}
+            />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4}>
+          <FormGroup>
+            <Label for="gender">Gender</Label>
+            <br />
+            <Row>
+              <Col md={2}>
+                <Input
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  checked={props.statedata.gender === "male"}
+                  onChange={props.changefunct}
+                />
+                Male
+              </Col>
+              <Col md={2}>
+                <Input
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  checked={props.statedata.gender === "female"}
+                  onChange={props.changefunct}
+                />
+                Female
+              </Col>
+            </Row>
+          </FormGroup>
+        </Col>
+        <Col md={4}>
+          <FormGroup>
+            <Label for="hob">Hob</Label>
+            <br />
+            <Row>
+              {checkboxes.map((item, index) => (
+                <Col md={2}>
+                  <label>
+                    {item.label}
+                    <Input
+                      type="checkbox"
+                      key={index}
+                      name={item.name}
+                      value={item.value}
+                      // checked={props.statedata.checkedItems.get(item.name)}
+                      onChange={props.changefunct}
+                    />
+                  </label>
+                </Col>
+              ))}
+            </Row>
+          </FormGroup>
+        </Col>
+        <Col md={4}>
+          <FormGroup>
+            <Label for="edu">Edu</Label>
+            <Select
+              name="edu"
+              value={props.statedata.edu}
+              onChange={props.SelectChange}
+              options={options}
+            />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4}>
+          <FormGroup>
+            <Label for="npa_date">Select Date</Label>
+            <DatePicker
+              name="npa_date"
+              dateFormat="yyyy-MM-dd"
+              placeholderText="Select NPA date ..."
+              selected={
+                props.statedata.npa_date
+                  ? Date.parse(props.statedata.npa_date)
+                  : ""
+              }
+              onChange={props.handleDate.bind(this, "npa_date")}
+            />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Col md={4}>
         <Input type="submit" />
-      </Form>
-    );
-}
+      </Col>
+    </Form>
+  );
+};
 
 export default UserForm;
